49190f44f418fa37aa2e4426679ad6c8f1f40f99
erikmura44 <erikmura44@gmail.com>
2017-04-11 13:32:27 -0600

    added user prompt for guess

--- Hangman.java
+++ Hangman.java
@@ -1,7 +1,15 @@
 public class Hangman {
 
-  public static void main(String[] args){
+  public static void main(String[] args) {
+    // Your incredible code goes here...
 
     Game game = new Game("treehouse");
+    Prompter prompter = new Prompter(game);
+    boolean isHit = prompter.promptForGuess();
+    if (isHit) {
+      System.out.println("We got a hit");
+    } else {
+      System.out.println("We got a miss");
+    }
   }
 }
