4ba08c501a49d35d86bd133542c90478c7797d02
erikmura44 <erikmura44@gmail.com>
2017-04-11 14:18:44 -0600

    updated to use remaining tries loop

--- Hangman.java
+++ Hangman.java
@@ -5,11 +5,11 @@ public class Hangman {
 
     Game game = new Game("treehouse");
     Prompter prompter = new Prompter(game);
-    boolean isHit = prompter.promptForGuess();
-    if (isHit) {
-      System.out.println("We got a hit");
-    } else {
-      System.out.println("We got a miss");
+
+    while(game.getRemainingTries() > 0 ){
+      prompter.displayProgress();
+      prompter.promptForGuess();
     }
+
   }
 }
