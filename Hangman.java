e345abc45d326de4a735455f3b0e912de220202f
erikmura44 <erikmura44@gmail.com>
2017-04-11 15:44:20 -0600

    changed so user inputs answer

--- Hangman.java
+++ Hangman.java
@@ -3,13 +3,19 @@ public class Hangman {
   public static void main(String[] args) {
     // Your incredible code goes here...
 
-    Game game = new Game("treehouse");
+    if(args.length == 0){
+      System.out.println("Usage: java Hangman <answer>");
+      System.out.println("answer is required");
+      System.exit(1);
+    }
+
+    Game game = new Game(args[0]);
     Prompter prompter = new Prompter(game);
 
-    while(game.getRemainingTries() > 0 ){
+    while(game.getRemainingTries() > 0 && !game.isWon()){
       prompter.displayProgress();
       prompter.promptForGuess();
     }
-
+    prompter.displayOutcome();
   }
 }
